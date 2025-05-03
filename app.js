8d55e6bb0c53ccb102d3e5b842213c7952a2b42a
Zen-Coderon <gaganreddyedu@gmail.com>
2025-05-03 16:24:11 +0530

    structured images and added nums

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const base = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/p
 
 for(let i=1;i<=10;i++){
     const newImg = document.createElement('img');
+    const pokemon = document.createElement('div');
+    const num = document.createElement('span');
     newImg.src = `${base}${i}.png`;
-    container.appendChild(newImg);
+    num.innerText=`#${i}`;
+    pokemon.appendChild(newImg);
+    pokemon.appendChild(num);
+    container.appendChild(pokemon);
 }
